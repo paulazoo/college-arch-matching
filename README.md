862ead3cea6fbb98cb7399901e43b551b6bf44f6
paulazoo <paulazhu@college.harvard.edu>
2022-07-19 19:18:28 -0400

    todo + no more triple mentees

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # College ARCH matching
 College ARCH mentor and mentee matching using linear simplex method written in matlab by paula zhu
+
+
+## TODO
+- after frontend/backend addition, account for mentee's importance ratings
+- account for essay length
--- preprocess_data.m
+++ preprocess_data.m
@@ -9,16 +9,14 @@ original_mentor_applicants = mentor_applicants;
 % Duplicate mentor rows for mentors willing to accept multiple mentees
 consider_multi_mentees = 1;
 
+% ONLY WORKS UP TO 2 MULTIMENTEES (trying to avoid triple mentees)
 if consider_multi_mentees == 1
     [total_mentors, ~] = size(mentor_applicants);
     for i = 1:1:total_mentors
         multi_mentees = table2array(mentor_applicants(i, 11));
         if multi_mentees > 1
-
-            for j = 1:1:(multi_mentees - 1) % For every other mentee they can take
-                mentor_row = mentor_applicants(i, :);
-                mentor_applicants = [mentor_applicants; mentor_row]; % Add these rows to the bottom
-            end
+            mentor_row = mentor_applicants(i, :);
+            mentor_applicants = [mentor_applicants; mentor_row]; % Add these rows to the bottom
         end
     end
 end
